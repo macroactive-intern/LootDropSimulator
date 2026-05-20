884f00b652def6cb8affbe6c22a3e169082a18d7
Nathan McCormick <mccormicknathan28@gmail.com>
2026-05-21 09:44:02 +1200

    Use Sanctum auth for loot API routes

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/loot-drops/global-stats', [LootController::class, 'globalStats']);
 
-Route::middleware('auth')->group(function (): void {
+Route::middleware('auth:sanctum')->group(function (): void {
     Route::post('/loot-drop', [LootController::class, 'store']);
     Route::get('/loot-drops', [LootController::class, 'index']);
     Route::get('/loot-drops/stats', [LootController::class, 'stats']);
