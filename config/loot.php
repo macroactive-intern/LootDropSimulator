916206b3fc57cfbf1771021449f84e4d94b563ee
Nathan McCormick <mccormicknathan28@gmail.com>
2026-05-21 08:39:14 +1200

    Add loot item configuration

---
+++ config/loot.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'guild_leader_legendary_multiplier' => 2.0,
+
+    'items' => [
+        [
+            'name' => 'Common Sword',
+            'weight' => 60,
+            'rarity' => 'common',
+            'stackable' => false,
+            'max_stack' => 1,
+        ],
+        [
+            'name' => 'Rare Shield',
+            'weight' => 25,
+            'rarity' => 'rare',
+            'stackable' => false,
+            'max_stack' => 1,
+        ],
+        [
+            'name' => 'Epic Helm',
+            'weight' => 10,
+            'rarity' => 'epic',
+            'stackable' => false,
+            'max_stack' => 1,
+        ],
+        [
+            'name' => 'Legendary Ring',
+            'weight' => 5,
+            'rarity' => 'legendary',
+            'stackable' => false,
+            'max_stack' => 1,
+        ],
+    ],
+];
