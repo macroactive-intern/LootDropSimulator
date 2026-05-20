67a593039c1ee139b0d151b6e003a04b7e50ed36
Nathan McCormick <mccormicknathan28@gmail.com>
2026-05-21 08:58:30 +1200

    Add loot dropped event

---
+++ app/Events/LootDropped.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\DroppedItem;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class LootDropped
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public DroppedItem $droppedItem,
+    )
+    {
+    }
+}
